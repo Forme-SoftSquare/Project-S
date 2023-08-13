47462ab798e38a1e51e618010533c1180d32a88d
Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>
2023-08-13 20:42:14 +0200

    feat: add basic player movements (#4)
    * feat: add basic player movements
    
    * refactor: rb2D to rb
    
    * feat: split basic movements in files
    
    * feat: add player collision in controller
    
    ---------
    
    Co-authored-by: Léon Dumestre <leon.dumestre@gmail.com>

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController playerController;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If player collides with a platform, set isJumping to false
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            playerController.playerMovement.isJumping = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        // If player exits a platform, set isJumping to true
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            playerController.playerMovement.isJumping = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] internal PlayerInput playerInput;
+    [SerializeField] internal PlayerMovement playerMovement;
+    [SerializeField] internal PlayerCollision playerCollision;
+
+    internal Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+}
