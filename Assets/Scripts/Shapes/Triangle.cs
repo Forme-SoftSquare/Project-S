5db83c212c24604ec4da7e03f90ce0dff840c63f
Fina49 <elie.debeyeler@gmail.com>
2023-08-22 14:47:25 +0200

    feat: :sparkles: Dash upward

--- Assets/Scripts/Shapes/Triangle.cs
+++ Assets/Scripts/Shapes/Triangle.cs
@@ -47,24 +47,46 @@ public class Triangle : Shape
 
     private IEnumerator Dash()
     {
-        Direction direction = playerController.playerMovement.direction;
+        // Determine the current horizontal direction the player is facing (either Left or Right)
+        Direction horizontalDirection = playerController.playerMovement.direction;
 
-        // Apllying visual effects
-        playerController.spriteRenderer.color = Color.red;
-        playerController.trailRenderer.emitting = true;
+        // Check if the 'up' input is currently pressed
+        bool isUpPressed = playerController.playerInput.isUpPressed;
 
-        Vector2 dashDirection = (direction == Direction.Left) ? Vector2.left : Vector2.right;
+        // Applying visual effects for dashing
+        playerController.spriteRenderer.color = Color.red;           // Change player color to red for the dash duration
+        playerController.trailRenderer.emitting = true;              // Turn on the trail renderer to show a "dash trail"
+
+        Vector2 dashDirection;
+
+        // Determine the dash direction based on the player's current horizontal direction and if 'up' is being pressed
+        if (horizontalDirection == Direction.Left)
+        {
+            // If the player is facing left and pressing 'up', they will dash diagonally upwards to the left
+            // Otherwise, they'll dash straight to the left
+            dashDirection = isUpPressed ? new Vector2(-1, 1).normalized : Vector2.left;
+        }
+        else // If horizontalDirection is Right
+        {
+            // If the player is facing right and pressing 'up', they will dash diagonally upwards to the right
+            // Otherwise, they'll dash straight to the right
+            dashDirection = isUpPressed ? new Vector2(1, 1).normalized : Vector2.right;
+        }
+
+        // Apply the dash velocity to the player's Rigidbody
         playerController.rb.velocity = dashDirection * dashForce;
 
-        // Wait for the dash to end
+        // Wait for a set duration (dashDuration) before ending the dash
         yield return new WaitForSeconds(dashDuration);
 
-        // Resetting visual effects
-        playerController.spriteRenderer.color = Color.blue;
-        playerController.trailRenderer.emitting = false;
-        
+        // Reset the visual effects after dashing
+        playerController.spriteRenderer.color = Color.blue;          // Reset player color back to blue after dashing
+        playerController.trailRenderer.emitting = false;             // Turn off the trail renderer
+
+        // Set the movement skill flag to false, indicating that the dash has ended
         isMovementSkillActive = false;
 
+        // Check if the player was jumping while dashing, and set a flag if true
         if (playerController.playerMovement.isJumping)
         {
             hasDashedInAir = true;
