dd0dd184f9969e340cf34a4d24872da4aad16b8b
Fina49 <108332166+Fina49@users.noreply.github.com>
2023-08-22 18:06:20 +0200

    Fix dash (#14)
    * feat: :sparkles: Dash upward
    
    * fix: diagonal dash if up btn held
    
    * Update Assets/Scripts/Shapes/Triangle.cs
    
    Co-authored-by: Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leon Dumestre <ldumestre@groupeastek.fr>
    Co-authored-by: Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,7 @@ public class PlayerInput : MonoBehaviour
 
     internal bool isLeftHeld;
     internal bool isRightHeld;
+    internal bool isUpHeld;
     internal bool isUpPressed;
     internal bool isUpReleased;
     internal bool isDownHeld;
@@ -29,6 +30,7 @@ public class PlayerInput : MonoBehaviour
     {
         isLeftHeld = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
         isRightHeld = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        isUpHeld = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space);
         isUpPressed = Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space);
         isUpReleased = Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space);
         isDownHeld = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
--- Assets/Scripts/Shapes/Triangle.cs
+++ Assets/Scripts/Shapes/Triangle.cs
@@ -47,24 +47,43 @@ public class Triangle : Shape
 
     private IEnumerator Dash()
     {
-        Direction direction = playerController.playerMovement.direction;
+        // Determine the current horizontal direction the player is facing (either Left or Right)
+        Direction horizontalDirection = playerController.playerMovement.direction;
 
-        // Apllying visual effects
-        playerController.spriteRenderer.color = Color.red;
-        playerController.trailRenderer.emitting = true;
+        // Check if the 'up' input is currently pressed
+        bool isUpHeld = playerController.playerInput.isUpHeld;
 
-        Vector2 dashDirection = (direction == Direction.Left) ? Vector2.left : Vector2.right;
+        // Applying visual effects for dashing
+        playerController.spriteRenderer.color = Color.red;           // Change player color to red for the dash duration
+        playerController.trailRenderer.emitting = true;              // Turn on the trail renderer to show a "dash trail"
+
+        // Determine the horizontal multiplier based on the player's direction
+        int horizontalMultiplier = (horizontalDirection == Direction.Left) ? -1 : 1;
+
+        // Determine the dash direction
+        Vector2 dashDirection = Vector2.right * horizontalMultiplier;
+
+        // If 'up' is held, adjust the dash direction diagonally
+        if (isUpHeld)
+        {
+            dashDirection += Vector2.up;
+            dashDirection.Normalize();
+        }
+
+        // Apply the dash velocity to the player's Rigidbody
         playerController.rb.velocity = dashDirection * dashForce;
 
-        // Wait for the dash to end
+        // Wait for a set duration (dashDuration) before ending the dash
         yield return new WaitForSeconds(dashDuration);
 
-        // Resetting visual effects
-        playerController.spriteRenderer.color = Color.blue;
-        playerController.trailRenderer.emitting = false;
-        
+        // Reset the visual effects after dashing
+        playerController.spriteRenderer.color = Color.blue;          // Reset player color back to blue after dashing
+        playerController.trailRenderer.emitting = false;             // Turn off the trail renderer
+
+        // Set the movement skill flag to false, indicating that the dash has ended
         isMovementSkillActive = false;
 
+        // Check if the player was jumping while dashing, and set a flag if true
         if (playerController.playerMovement.isJumping)
         {
             hasDashedInAir = true;
